8f69ba95050bc9361105bc585a2f519c28300588
Subrahmanyam pampana <subrahmanyam.pampana.28@gmail.com>
2022-02-21 11:22:59 +0530

    Create imageProcessingNotes.cmd

---
+++ imageProcessingNotes.cmd
@@ -0,0 +1,3 @@
+# INSTALLATION OF NUMPY AND OPEN CV
+Download the packages from an unofficial url : ```http://www.lfd.uci.edu/~gohlke/pythonlibs/#numpy```
+downlaod this file ```numpy‑1.22.2+mkl‑cp38‑cp38‑win32.whl``` 
